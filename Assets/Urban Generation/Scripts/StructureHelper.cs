15cca62a7898a48807f9f245b2dc450317c93821
HugoJ0 <73164643+HugoJ0@users.noreply.github.com>
2023-05-25 20:35:42 +0200

    Experimenting on adding structure around road

---
+++ Assets/Urban Generation/Scripts/StructureHelper.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructureHelper : MonoBehaviour
+{
+    public GameObject prefab;
+    public Dictionary<Vector3Int, GameObject> structureDictionary = new Dictionary<Vector3Int, GameObject>();
+
+    public void PlaceStructureAroundRoad(List<Vector3Int> roadPositions)
+    {
+        Dictionary<Vector3Int, Direction> freeEstateSpots = FindFreeSpacesAroundRoad(roadPositions);
+        foreach (var position in freeEstateSpots.Keys)
+        {
+            Instantiate(prefab, position, Quaternion.identity, transform);
+        }
+    }
+
+    private Dictionary<Vector3Int, Direction> FindFreeSpacesAroundRoad(List<Vector3Int> roadPositions)
+    {
+        Dictionary<Vector3Int, Direction> freeSpaces = new Dictionary<Vector3Int, Direction>();
+        foreach (var position in roadPositions)
+        {
+            var neighbourDirections = PlacementHelper.FindNeighbour(position, roadPositions);
+            foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+            {
+                if(neighbourDirections.Contains(direction) == false)
+                {
+                    var newPosition = position + PlacementHelper.GetOffsetFromDirection(direction);
+                    if(freeSpaces.ContainsKey(newPosition))
+                    {
+                        continue;
+                    }
+                    freeSpaces.Add(newPosition, Direction.Right);
+                }
+            }
+        }
+        return freeSpaces;
+    }
+}
